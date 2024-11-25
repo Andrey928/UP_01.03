cea838906724cd426ddb89c706b050c398fec32b
Andrey928 <suskov.andrey2004@mail.ru>
2024-11-25 22:54:03 +0300

    first commit

---
+++ app/src/main/java/com/example/quiz/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.quiz
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
+        }
+
+    }
+
+    fun btn_click(view: View) {
+        val textbox = findViewById<EditText>(R.id.et_name)
+        if (textbox.text.toString().isEmpty()){
+            Toast.makeText(this, "Пожалуйста, введите свое имя", Toast.LENGTH_SHORT).show()
+
+        }else{
+            val intent = Intent(this, QuizQuestionsActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/quiz/QuizQuestionsActivity.kt
@@ -0,0 +1,193 @@
+package com.example.quiz
+
+import android.content.Context
+import android.graphics.Color
+import android.graphics.Typeface
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_quiz_questions)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val tv_option_one = findViewById<TextView>(R.id.tv_option_one)
+        val tv_option_two = findViewById<TextView>(R.id.tv_option_two)
+        val tv_option_three = findViewById<TextView>(R.id.tv_option_three)
+        val tv_option_four = findViewById<TextView>(R.id.tv_option_four)
+        val btn_submit = findViewById<Button>(R.id.btn_submit)
+
+        mQuestionsList = Constants.getQuestions()
+
+        setQuestion()
+
+        tv_option_one.setOnClickListener(this)
+        tv_option_two.setOnClickListener(this)
+        tv_option_three.setOnClickListener(this)
+        tv_option_four.setOnClickListener(this)
+        btn_submit.setOnClickListener(this)
+
+
+        
+    }
+
+    private fun setQuestion() {
+        val btn_submit = findViewById<Button>(R.id.btn_submit)
+        val tv_option_one = findViewById<TextView>(R.id.tv_option_one)
+        val tv_option_two = findViewById<TextView>(R.id.tv_option_two)
+        val tv_option_three = findViewById<TextView>(R.id.tv_option_three)
+        val tv_option_four = findViewById<TextView>(R.id.tv_option_four)
+        val iv_image = findViewById<ImageView>(R.id.iv_image)
+        val tv_question = findViewById<TextView>(R.id.tv_question)
+        val tv_progress = findViewById<TextView>(R.id.tv_progress)
+        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
+
+        val question = mQuestionsList!![mCurrentPosition-1]
+
+        defaultOptionsView()
+
+        if (mCurrentPosition == mQuestionsList!!.size){
+            btn_submit.text = "Завершить"
+        }else{
+            btn_submit.text = "Подтвердить"
+        }
+
+        progressBar.progress = mCurrentPosition
+        tv_progress.text = "$mCurrentPosition" + "/" + progressBar.max
+
+        tv_question.text = question!!.question
+        iv_image.setImageResource(question.image)
+        tv_option_one.text = question.optionOne
+        tv_option_two.text = question.optionTwo
+        tv_option_three.text = question.optionThree
+        tv_option_four.text = question.optionFour
+    }
+    private fun defaultOptionsView(){
+        val options = ArrayList<TextView>()
+        val tv_option_one = findViewById<TextView>(R.id.tv_option_one)
+        val tv_option_two = findViewById<TextView>(R.id.tv_option_two)
+        val tv_option_three = findViewById<TextView>(R.id.tv_option_three)
+        val tv_option_four = findViewById<TextView>(R.id.tv_option_four)
+        options.add(0, tv_option_one)
+        options.add(1, tv_option_two)
+        options.add(2, tv_option_three)
+        options.add(3, tv_option_four)
+        for (option in options){
+            option.setTextColor(Color.parseColor("#7A8089"))
+            option.typeface = Typeface.DEFAULT
+            option.background = ContextCompat.getDrawable(this, R.drawable.default_option_border_bg)
+        }
+    }
+
+    private var mCurrentPosition:Int = 1
+    private var mQuestionsList: ArrayList<Question>? = null
+    private var mSelectedOptionPosition :Int = 0
+
+    override fun onClick(v: View?) {
+        val btn_submit = findViewById<Button>(R.id.btn_submit)
+        val tv_option_one = findViewById<TextView>(R.id.tv_option_one)
+        val tv_option_two = findViewById<TextView>(R.id.tv_option_two)
+        val tv_option_three = findViewById<TextView>(R.id.tv_option_three)
+        val tv_option_four = findViewById<TextView>(R.id.tv_option_four)
+        when(v?.id){
+        R.id.tv_option_one ->{
+            selectedOptionView(tv_option_one, 1)
+        }
+        R.id.tv_option_two -> {
+            selectedOptionView(tv_option_two,2)
+
+        }
+            R.id.tv_option_three -> {
+                selectedOptionView(tv_option_three, 3)
+
+            }
+            R.id.tv_option_four -> {
+                selectedOptionView(tv_option_four, 4)
+            }
+            R.id.btn_submit -> {
+                if(mSelectedOptionPosition == 0){
+                    mCurrentPosition++
+
+                    when{
+                        mCurrentPosition <= mQuestionsList!!.size -> {
+                            setQuestion()
+                        }else ->{
+                            Toast.makeText(this, "Вы успешно прошли викторину", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }else{
+                    val question = mQuestionsList?.get(mCurrentPosition - 1)
+
+                    if (question!!.correctAnswer!=mSelectedOptionPosition){
+                        answerView(mSelectedOptionPosition, R.drawable.wrong_option_border_bg)
+                    }
+                    answerView(question.correctAnswer, R.drawable.correct_option_border_bg)
+
+                    if (mCurrentPosition == mQuestionsList!!.size){
+                        btn_submit.text = "Завершить"
+                    }else{
+                        btn_submit.text = "Следующий вопрос"
+                    }
+                    mSelectedOptionPosition = 0
+                }
+            }
+
+    }
+    }
+    private fun answerView(answer:Int, drawableView: Int) {
+        val tv_option_one = findViewById<TextView>(R.id.tv_option_one)
+        val tv_option_two = findViewById<TextView>(R.id.tv_option_two)
+        val tv_option_three = findViewById<TextView>(R.id.tv_option_three)
+        val tv_option_four = findViewById<TextView>(R.id.tv_option_four)
+        when(answer){
+
+            1->{
+                tv_option_one.background = ContextCompat.getDrawable(
+                    this, drawableView
+                )
+            }
+            2->{
+                tv_option_two.background = ContextCompat.getDrawable(
+                    this, drawableView
+                )
+            }
+            3->{
+                tv_option_three.background = ContextCompat.getDrawable(
+                    this, drawableView
+                )
+            }
+            4->{
+                tv_option_four.background = ContextCompat.getDrawable(
+                    this, drawableView
+                )
+            }
+
+        }
+    }
+    private fun selectedOptionView(textView: TextView, selectedOptionNum: Int){
+        defaultOptionsView()
+        mSelectedOptionPosition = selectedOptionNum
+        textView.setTextColor(Color.parseColor("#363A43"))
+        textView.setTypeface(textView.typeface, Typeface.BOLD)
+        textView.background = ContextCompat.getDrawable(this, R.drawable.selected_option_border_bg)
+
+    }
+
+}
+
